55c8e7d944034342dd0c2285c7d38cf7f69c4759
Chunhawit <pupz28@gmail.com>
2020-04-13 04:29:02 +0200

    added function to filter price, breakfast, pets

--- src/context.js
+++ src/context.js
@@ -59,7 +59,7 @@ class RoomProvider extends Component {
 
   handleChange = event => {
     const target = event.target;
-    const value = event.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = event.target.name;
     this.setState(
       {
@@ -86,6 +86,7 @@ class RoomProvider extends Component {
 
     //transform value
     capacity = parseInt(capacity);
+    price = parseInt(price);
 
     // filter by type
     if (type !== 'all') {
@@ -97,6 +98,25 @@ class RoomProvider extends Component {
       tempRooms = tempRooms.filter(room => room.capacity >= capacity);
     }
 
+    // filter by price
+    tempRooms = tempRooms.filter(room => room.price < price);
+
+    // filter by room size
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
+
+    // filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true);
+    }
+
+    // filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
+
+    // change state
     this.setState({
       sortedRooms: tempRooms,
     });
